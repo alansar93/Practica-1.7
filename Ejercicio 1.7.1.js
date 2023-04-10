b537b24c28bd11d55708dfa99f94f6e17dea427c
Alan  Sarchman <alansarhchman@live.com.argit config --global user.emailgit config --globalgit config --global config user.namegit initgit config --global user.email alansarhchman@live.com.ar>
2023-04-10 20:11:56 -0300

    Nueva Practica con facu

---
+++ Ejercicio 1.7.1.js
@@ -0,0 +1,7 @@
+const MANZANA_MARIA = 5;
+const MANZANA_PEDRO = 8;
+const MANZANA_SUSANA = 9;
+
+let ManzanaTotal = MANZANA_MARIA + MANZANA_PEDRO + MANZANA_SUSANA;
+
+console.log("hay " + ManzanaTotal + " manzanas en total");
---
+++ Ejercicio 1.7.2.js
@@ -0,0 +1,42 @@
+const CANT_MAGD_JUAN = 28;
+const CANT_AMIGOS = 12;
+
+let cantManzana = 0;
+
+console.log("Quedan " + cantManzana + " manzanas en total");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
