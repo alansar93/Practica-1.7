15edbd8a29ac4e15a0092be0227bcda944288bfa
Alan Sarchman <alansarchman@live.com.ar>
2023-04-14 11:09:09 -0300

    actualizado

---
+++ Ejercicio1.7.4.js
@@ -0,0 +1,7 @@
+const CANT_GALLETAS_TOTAL = 5;
+
+let cajasCompradas = 4;
+let galletasTotal = (CANT_GALLETAS_TOTAL * cajasCompradas);
+
+console.log("Tenemos " + galletasTotal + " de galletas");
+
---
+++ Ejercico 1.7.5.js
@@ -0,0 +1,8 @@
+const PUEBLO_A_TOLED0 = 2600;
+const TIENDA_A_TIO = 691;
+
+let totalRecorrido = (PUEBLO_A_TOLED0 + TIENDA_A_TIO);
+let difMilimetroAMetro = 1000;
+let TotalEnMilimetro = (totalRecorrido * difMilimetroAMetro);
+
+console.log("en total recorrio " + TotalEnMilimetro + " milimetros");
